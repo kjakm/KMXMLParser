3e50be6b6c4c93cc4790983430f5c50255726aa7
Kieran McGrady <kieran@hotrodsoftware.com>
2013-05-09 19:41:32 +0100

    First commit

---
+++ KMRSS/KMRSS/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  KMRSS
+//
+//  Created by Kieran McGrady on 09/05/2013.
+//  Copyright (c) 2013 HotRod software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ KMRSS/KMRSS/WebViewController.m
@@ -0,0 +1,38 @@
+//
+//  WebViewController.m
+//  KMRSS
+//
+//  Created by Kieran McGrady on 09/05/2013.
+//  Copyright (c) 2013 HotRod software. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
